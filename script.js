92df67377f55ec86c9bbb2ffca68aac67487532c
PeiaKassio <82101834+PeiaKassio@users.noreply.github.com>
2024-11-08 18:10:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicGVpc2thc3NpbyIsImEiOiJjbTM4eHB5NHIwd2M5Mmlxe
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10', // Use a Mapbox style 'mapbox://styles/peiskassio/cm38wege300j601pd4oe2f3re'
+    style: 'mapbox://styles/peiskassio/cm38wege300j601pd4oe2f3re', //  'mapbox://styles/mapbox/dark-v10'
     center: [0, 0], // Center coordinates [longitude, latitude]
     zoom: 1.5, // Start at a zoom level appropriate for a globe view
     projection: 'globe' // Enable globe projection
