224bc6fcfdf039d24274831cf77ec75174b4607c
PeiaKassio <82101834+PeiaKassio@users.noreply.github.com>
2024-11-09 11:25:03 +0100

    Update data-analysis.js

--- data-analysis.js
+++ data-analysis.js
@@ -1,44 +1,50 @@
 import continentMapping from './continentMapping.js';
 
 async function loadData() {
-    const response = await fetch('artwork-data.json');
-    const data = await response.json();
+    try {
+        const response = await fetch('artwork-data.json');
+        const data = await response.json();
+        console.log('Loaded data:', data); // Debugging line to check data
 
-    const tagsByCountry = {};
-    const tagsByContinent = {};
-    const focusShiftByContinent = {};
+        const tagsByCountry = {};
+        const tagsByContinent = {};
+        const focusShiftByContinent = {};
 
-    data.features.forEach((feature) => {
-        const location = feature.properties.location;
-        const country = location.split(', ').pop();
-        const continent = continentMapping[country] || "Other";
-        const topics = feature.properties.tags.topic || [];
-        const year = feature.properties.year || "Unknown";
+        data.features.forEach((feature) => {
+            const location = feature.properties.location;
+            const country = location.split(', ').pop();
+            const continent = continentMapping[country] || "Other";
+            const topics = feature.properties.tags.topic || [];
+            const year = feature.properties.year || "Unknown";
 
-        // Count topics by country
-        if (!tagsByCountry[country]) tagsByCountry[country] = {};
-        topics.forEach(topic => {
-            tagsByCountry[country][topic] = (tagsByCountry[country][topic] || 0) + 1;
+            // Count topics by country
+            if (!tagsByCountry[country]) tagsByCountry[country] = {};
+            topics.forEach(topic => {
+                tagsByCountry[country][topic] = (tagsByCountry[country][topic] || 0) + 1;
+            });
+
+            // Count topics by continent
+            if (!tagsByContinent[continent]) tagsByContinent[continent] = {};
+            topics.forEach(topic => {
+                tagsByContinent[continent][topic] = (tagsByContinent[continent][topic] || 0) + 1;
+            });
+
+            // Track environmental focus shift by continent over time
+            if (!focusShiftByContinent[continent]) focusShiftByContinent[continent] = {};
+            if (!focusShiftByContinent[continent][year]) focusShiftByContinent[continent][year] = {};
+            topics.forEach(topic => {
+                focusShiftByContinent[continent][year][topic] = 
+                    (focusShiftByContinent[continent][year][topic] || 0) + 1;
+            });
         });
 
-        // Count topics by continent
-        if (!tagsByContinent[continent]) tagsByContinent[continent] = {};
-        topics.forEach(topic => {
-            tagsByContinent[continent][topic] = (tagsByContinent[continent][topic] || 0) + 1;
-        });
-
-        // Track environmental focus shift by continent over time
-        if (!focusShiftByContinent[continent]) focusShiftByContinent[continent] = {};
-        if (!focusShiftByContinent[continent][year]) focusShiftByContinent[continent][year] = {};
-        topics.forEach(topic => {
-            focusShiftByContinent[continent][year][topic] = 
-                (focusShiftByContinent[continent][year][topic] || 0) + 1;
-        });
-    });
-
-    createTagsByCountryChart(tagsByCountry);
-    createTagsByContinentChart(tagsByContinent);
-    createFocusShiftOverTimeChart(focusShiftByContinent);
+        // Generate the charts
+        createTagsByCountryChart(tagsByCountry);
+        createTagsByContinentChart(tagsByContinent);
+        createFocusShiftOverTimeChart(focusShiftByContinent);
+    } catch (error) {
+        console.error('Error loading or processing data:', error);
+    }
 }
 
 // Create bar chart for topics by country
